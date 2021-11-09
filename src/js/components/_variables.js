01b19aaf37d1d5cb2c3f1c3c91b0d5b9be05131c
grafit_off <graf.off.it@gmail.com>
2021-11-09 18:12:22 +0200

    feat: add eleventy.js + njk, change folder structure

--- src/js/components/_variables.js
+++
@@ -1,9 +0,0 @@
-// Variables
-const body = document.querySelector('.body');
-const isMobileDevice = (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent));
-const isSmoothScrollSupported = 'scrollBehavior' in document.documentElement.style;
-const myBurger = document.querySelector('.burger');
-const header = document.querySelector('.header');
-const nav = document.querySelector('.nav');
-const burgerCl = new Burger(myBurger, nav);
-const themeSwitcher = document.querySelector('.header__theme-switcher');
